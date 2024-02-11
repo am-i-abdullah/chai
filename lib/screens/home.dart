5217a764f35a83b76ad349ffa07bacbc32aea3e1
am-i-abdullah <abdullah094995@email.com>
2024-02-11 23:31:48 +0500

    home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,36 +1,79 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:chai/screens/booking.dart';
+import 'package:chai/screens/orders.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _screenIndex = 0;
+
+  final _screens = [
+    const BookingScreen(),
+    const OrdersScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xffEEDCC6),
-          foregroundColor: const Color.fromRGBO(35, 12, 2, 1),
-          title: const Text(
-            "Good Afternoon, Bae",
-            style: TextStyle(fontSize: 18),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.menu),
-            )
-          ],
+      appBar: AppBar(
+        backgroundColor: const Color(0xffEEDCC6),
+        foregroundColor: const Color.fromRGBO(35, 12, 2, 1),
+        title: const Text(
+          "Good Afternoon, Bae",
+          style: TextStyle(fontSize: 18),
         ),
-        body: Column(
-          children: [
-            Image.asset('assets/chai2.png'),
-            TextButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              child: const Text("log out"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu),
+          )
+        ],
+      ),
+      body: _screens[_screenIndex],
+      bottomNavigationBar: Container(
+        color: const Color.fromRGBO(46, 39, 47, 1),
+        padding: const EdgeInsets.all(10),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          showUnselectedLabels: true,
+          currentIndex: _screenIndex,
+          onTap: (index) {
+            setState(() {
+              _screenIndex = index;
+            });
+          },
+
+          elevation: 0,
+          // items style
+          selectedItemColor: Colors.white,
+          unselectedItemColor: const Color.fromRGBO(130, 125, 136, 1),
+
+          // labels styling
+          selectedLabelStyle: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 10,
+          ),
+          unselectedLabelStyle: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 10,
+          ),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.coffee),
+              label: 'Place Order',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              label: 'Orders',
             ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
